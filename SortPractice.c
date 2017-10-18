cf7bdb5b557f98f07d443cae217ca88de7e2edb5
rmweir <ricardo.weir1@gmail.com>
2017-10-17 22:01:54 -0700

    Started input

---
+++ SortPractice.c
@@ -0,0 +1,28 @@
+/*
+*I will use count sort to sort the given 
+*/
+#include <iostream>
+
+
+
+void startCountSort() {
+    
+    vector<int> toBeSorted;
+    unsigned numberOfInputs;
+    
+    std::cout << "How many ints will you enter? ";
+    
+    for(int count = 0; count < numberOfInputs; count++) {
+        std::cin >> numberOfInputs;
+        toBeSort.push_back(numberOfInputs);
+    }
+    
+       
+    
+}
+
+int main() {
+
+    startCountSort();
+
+}
