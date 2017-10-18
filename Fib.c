22d9ce7a52e0d67b23242c8df14d4f081ce68b85
rmweir <ricardo.weir1@gmail.com>
2017-10-17 21:21:14 -0700

    Working fib program with memoization and user input

---
+++ Fib.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+#include <iostream>
+#include <map>
+/* In this warmup we will create a fib program that utilizes
+   memoization.
+*/
+
+
+static std::map<unsigned, unsigned> memo;
+
+
+unsigned fib(unsigned n) {
+    
+    if (n < 2) 
+        return n;
+        
+    if(memo.count(n) > 0)
+        return memo[n];
+        
+    unsigned retValue = fib(n-1) + fib(n-2);
+    
+    memo[n] = retValue;
+    
+    return retValue;
+    
+}
+
+int main() {
+    unsigned input;
+    int valueToPrint;
+    
+    
+    printf("Please enter an argument for the fib sequence: ");
+    
+    std::cin >> input;
+    
+    valueToPrint = fib(input);
+    
+    printf("Value: %i\n", valueToPrint);
+    
+    
+    
+
+}
