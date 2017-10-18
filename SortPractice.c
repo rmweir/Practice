99075cd5aad21fe051a11ff8c985354c2a7ec380
rmweir <ricardo.weir1@gmail.com>
2017-10-17 22:11:51 -0700

    Error checking items collecting

--- SortPractice.c
+++ SortPractice.c
@@ -2,21 +2,30 @@
 *I will use count sort to sort the given 
 */
 #include <iostream>
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
 
-
+void PrintMe(int x) {
+    printf("X: %i\n", x);   
+    
+}
 
 void startCountSort() {
     
-    vector<int> toBeSorted;
+    std::vector<int> toBeSorted;
     unsigned numberOfInputs;
     
     std::cout << "How many ints will you enter? ";
+    std::cin >> numberOfInputs;
     
     for(int count = 0; count < numberOfInputs; count++) {
-        std::cin >> numberOfInputs;
-        toBeSort.push_back(numberOfInputs);
+        int addThisInput;
+        std::cin >> addThisInput;
+        toBeSorted.push_back(addThisInput);
     }
-    
+
+    for_each(toBeSorted.begin(), toBeSorted.end(), PrintMe);    
        
     
 }
