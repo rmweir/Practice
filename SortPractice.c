2242bdacc8a5e37a219a0cbf609b7a8729054c0e
rmweir <ricardo.weir1@gmail.com>
2017-10-19 20:00:29 -0700

    Finished step one in CountSort and create FindMax Function

--- SortPractice.c
+++ SortPractice.c
@@ -11,6 +11,35 @@ void PrintMe(int x) {
     
 }
 
+int FindMax(std::vector<int> x) {
+    return *std::max_element(x.begin(), x.end());
+    
+}
+
+void CountSort(std::vector<int> x) {
+    // Count array must be include highest value AND 0
+    // so size is FindMax(x) + 1
+    int count[FindMax(x) + 1];
+    int sorted[x.size()];
+    
+    for(int index = 0; index < x.size(); index++) {
+        int indexToInc = x[index];
+        count[indexToInc]++;
+    
+    }
+    
+    /*
+    for(int index = 0; index < count.size(); index++) {
+        if(count[index]) {
+            
+        }
+    
+    }*/
+    
+    
+
+}
+
 void startCountSort() {
     
     std::vector<int> toBeSorted;
@@ -20,12 +49,18 @@ void startCountSort() {
     std::cin >> numberOfInputs;
     
     for(int count = 0; count < numberOfInputs; count++) {
+    
         int addThisInput;
         std::cin >> addThisInput;
         toBeSorted.push_back(addThisInput);
+        
     }
-
-    for_each(toBeSorted.begin(), toBeSorted.end(), PrintMe);    
+    
+    int printMax = FindMax(toBeSorted);
+    printf("Max: %i", printMax);
+    
+    
+    //for_each(toBeSorted.begin(), toBeSorted.end(), PrintMe);    
        
     
 }
